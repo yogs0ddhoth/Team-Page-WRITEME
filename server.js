a3898d3203070427f47c30e5bfd235c40c2b3ca3
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-03-10 15:56:19 -0500

    prompts functional

--- server.js
+++ server.js
@@ -16,55 +16,41 @@ const generateMockUp = require('./lib/generateMockUp');
 
 const employees = [
   'Engineer',
-  'Intern',
-  'Finish'
+  'Intern'
 ]
 // base prompt question arrays:
-
-const managerPrompts = [
+const teamPrompts = [
   //* prompt team manager information
-  new Input('employees.manager.name', 'Enter Manager Name:'),
-  new Input('employees.manager.email', 'Enter email:', 
+  new Input('manager.name', 'Enter Manager Name:'),
+  new Input('manager.id', "Enter Employee Id:"),
+  new Input('manager.email', 'Enter email:', 
   // (answers) => // email verification function // ? true : 'error: Enter a valid email address'
   ),
-  new Input('employees.manager.id', "Enter Employee Id:"),
-  new Input('employees.manager.office', 'Enter office number:'),
+  new Input('manager.office', 'Enter office number:'),
+
+  //* Prompt-Loop
+  new Loop('team', 'Add team Member?', 
+  [
+    new List('type', 'Select a Team member:', employees),
+
+    //* conditional prompt for Engineer
+    new Input('name', 'Enter Engineer Name', (answers) => answers.type !== 'Intern'),
+    new Input('id', 'Enter Employee Id:', (answers) => answers.type !== 'Intern'),
+    new Input('email', 'Enter email:', (answers) => answers.type !== 'Intern'),
+    new Input('github', 'Enter GitHub username:', (answers) => answers.type !== 'Intern'),
+
+    //* prompts for Intern
+    new Input('name', 'Enter Intern Name', (answers) => answers.type !== 'Engineer'),
+    new Input('id', 'Enter Employee Id:', (answers) => answers.type !== 'Engineer'),
+    new Input('email', 'Enter email:', (answers) => answers.type !== 'Engineer'),
+    new Input('school', 'Enter School:', (answers) => answers.type !== 'Engineer'),
+  ])
 ];
-const [managerName, managerEmail, managerId, managerOffice] = managerPrompts;
-
-const teamPrompts = [
-  //* prompt select team members 
-  new List('type', 'Select a Team member:', employees),
-
-  //* conditional prompt for Engineer
-  new Input('name', 'Enter Engineer Name', 
-  (answers) => (answers.type !== 'Finish') && (answers.type !== 'Intern')
-  ),
-  new Input('id', 'Enter Employee Id:',  
-  (answers) => (answers.type !== 'Finish') && (answers.type !== 'Intern')
-  ),
-  new Input('email', 'Enter email:', 
-  (answers) => (answers.type !== 'Finish') && (answers.type !== 'Intern')
-  ),
-  new Input('github', 'Enter GitHub username:', 
-  (answers) => (answers.type !== 'Finish') && (answers.type !== 'Intern')),
-
-  //* prompts for Intern
-  new Input('name', 'Enter Name', 
-  (answers) => (answers.type !== 'Finish') && (answers.type !== 'Engineer')
-  )
-];
-const teamLoop = {
-  type: 'loop',
-  name: 'team', 
-  message: 'Add Team Member?', 
-  questions: teamPrompts
-};
 
 // Main functionality:
 const init = () => {
   // prompt answers to generateMarkdown()
-  inquirer.prompt(teamLoop).then((answers) => {
+  inquirer.prompt(teamPrompts).then((answers) => {
     console.log(JSON.stringify(answers)); // for debugging purposes
     
     // // write the HTML from mockup 
@@ -76,6 +62,6 @@ const init = () => {
 
 // Function call to initialize app
 init();
-
+// console.log(JSON.stringify(teamPrompts));
 // Export for testing:
 module.exports = init;
