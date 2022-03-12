079fcabb7671164679b9fe4fbed4f7c8ef617dc6
Ben Lin <95650934+yogs0ddhoth@users.noreply.github.com>
2022-03-12 17:50:51 -0500

    added validate functionality for email

--- server.js
+++ server.js
@@ -4,46 +4,41 @@ const inquirer = require('inquirer');
 inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer));
 
 // Include Modules:
-// //* classes:
-const Employee = require('./lib/Employee');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
 const {Input, List, Loop, Validate} = require('./lib/Prompt');
 // //* functions:
 const generateHTML = require('./lib/generateHTML');
 const {generateManager, generateEngineerHTML, generateInternHTML} = require('./lib/generateTeam')
 
-const employees = [
-  'Engineer',
-  'Intern'
-]
 // base prompt question arrays:
 const teamPrompts = [
   //* prompt team manager information
   new Input('manager.name', 'Enter Manager Name:'),
   new Input('manager.id', "Enter Employee Id:"),
-  new Input('manager.email', 'Enter email:', 
-  // (answers) => // email verification function // ? true : 'error: Enter a valid email address'
+  new Validate('manager.email', 'Enter email:', null,
+    (answer) => answer.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) ? true : 'Enter a valid email address'
   ),
   new Input('manager.office', 'Enter office number:'),
 
   //* Prompt-Loop
   new Loop('team', 'Add Team Member?', 
   [
-    new List('type', 'Select a Team member:', employees),
+    new List('type', 'Select a Team member:', ['Engineer', 'Intern']),
 
     //* conditional prompt for Engineer
     new Input('name', 'Enter Engineer Name', (answers) => answers.type !== 'Intern'),
     new Input('id', 'Enter Employee Id:', (answers) => answers.type !== 'Intern'),
-    new Input('email', 'Enter email:', (answers) => answers.type !== 'Intern'),
+    new Validate('email', 'Enter email:', (answers) => answers.type !== 'Intern', 
+      answer => answer.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) ? true : 'Enter a valid email address'
+    ),
     new Input('github', 'Enter GitHub username:', (answers) => answers.type !== 'Intern'),
 
     //* prompts for Intern
     new Input('name', 'Enter Intern Name', (answers) => answers.type !== 'Engineer'),
     new Input('id', 'Enter Employee Id:', (answers) => answers.type !== 'Engineer'),
-    new Input('email', 'Enter email:', (answers) => answers.type !== 'Engineer'),
-    new Input('school', 'Enter School:', (answers) => answers.type !== 'Engineer'),
+    new Validate('email', 'Enter email:', (answers) => answers.type !== 'Engineer', 
+      answer => answer.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/) ? true : 'Enter a valid email address'
+    ),
+    new Input('school', 'Enter School:', (answers) => answers.type !== 'Engineer')
   ])
 ];
 
@@ -55,16 +50,12 @@ const init = () => {
     const engineerSection = generateEngineerHTML(answers.team.filter(employee => employee.type === 'Engineer'));
     const internSection = generateInternHTML(answers.team.filter(employee => employee.type === 'Intern'));
 
-    console.log(generateHTML(managerSection, engineerSection, internSection));
-    // // write the HTML from mockup 
-    // fs.writeFile('./dist/index.html', generateHTML(answers), (err) => 
-    //   err ? console.log(err) : console.log(`Success! Check the 'dist' directory`)
-    // );
+    // write the HTML from mockup 
+    fs.writeFile('./dist/index.html', generateHTML(managerSection, engineerSection, internSection), (err) => 
+      err ? console.log(err) : console.log(`Success! Check the 'dist' directory`)
+    );
   })
 }
 
 // Function call to initialize app
 init();
-// console.log(JSON.stringify(teamPrompts));
-// Export for testing:
-module.exports = init;
